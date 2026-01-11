1312df8a44615254fc7fa6ef70b3553ec6cc0364
Michael Lesser <mlesser@arizona.edu>
2026-01-11 12:31:19 -0700

    add tools to optimize

--- imcon/optimize.py
+++ imcon/optimize.py
@@ -6,8 +6,11 @@ Everything here is imported to the imcon CLI.
 import azcam
 
 # set imports
-tools = azcam.db.tools
 pars = azcam.db.parameters
 api = azcam.db.api
 
-__all__ = ["azcam", "tools", "pars", "api"]
+__all__ = ["azcam", "pars", "api"]
+
+for obj in azcam.db.tools:
+    locals()[obj] = azcam.db.tools[obj]
+    __all__.append(obj)
